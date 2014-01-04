5ed8f775048635ad8492fc8cb7fddd138dedeade
svpernova09 <halo09@gmail.com>
2014-01-03 23:38:27 -0600

    adding install & uninstall

---
+++ bin/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir ~/.vim/backup/install-backup
+cp -r ~/.vimrc ~/.vim/backup/install-backup/.
+cp -r ~/.vim ~/.vim/backup/install-backup/.
+cp ~/.vim/vimrc ~/.vimrc
---
+++ bin/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r ~/.vim/backup/install-backup/.vimrc ~/.vimrc
+cp -r ~/.vim/backup/install-backup/.vim ~/.vim
+rm -rf ~/.vim/backup/install-backup
